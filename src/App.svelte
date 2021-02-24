6ccfbbe0b7cf890f592a1058ada2f435a6211d6f
Lukem121 <lukeask@hotmail.co.uk>
2021-02-24 03:10:07 +0000

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Tailwindcss from './Tailwindcss.svelte';
+	import Card from './components/Card.svelte';
+
+	let cards = new Array(13)
+	let prefix = "C"
+</script>
+
+<Tailwindcss />
+<main class="flex bg-green-500 h-screen items-center justify-center">
+
+	<Card value={"c1"}/>
+	<Card value={"h1"} bt={1}/>
+	<Card value={"s1"} bt={2}/>
+	<Card value={"d1"} bt={3}/>
+
+	<!-- {#each cards as card, i}
+		<Card value={prefix + (i+1)} />
+	{/each} -->
+</main>
+
+<style>
+</style>
---
+++ src/components/Card.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let value;
+    export let showBack = false;
+    export let bt = 0;
+
+    let path = value;
+    let backTypes = ["back1", "back2", "back3", "back4"]
+
+    const handleClick = () => {
+        showBack = !showBack;
+        if(showBack){
+            path = backTypes[bt];
+        }else{
+            path = value;
+        }
+    }
+
+</script>
+
+<div on:click={handleClick} class="cursor-pointer">
+    <img class="w-56 h-56 rounded" src="./cards/{path}.svg" alt="">
+</div>
